8f4410083ee91fc9c8d09ba9a418f2e8c5dfb4b5
Matheus-Godinho <matheus.brc22@gmail.com>
2020-01-22 01:59:59 -0300

    variáveis declaradas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> products;
+		Product product;
+		String name;
+		double price;
+		char option;
+		int n;
+		
+		sc.close();
 
 	}
 
