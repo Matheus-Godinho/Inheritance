ff5bca5aded86cc4a2e3695a44f41941be85faad
Matheus-Godinho <matheus.brc22@gmail.com>
2020-01-22 01:48:01 -0300

    Entidades implementadas

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class ImportedProduct {
+public final class ImportedProduct extends Product {
+	
+	private Double customsFee;
+
+	public ImportedProduct() {
+		super();
+	}
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public final String priceTag() {
+		return super.priceTag() 
+				+ " (Customs fee: $ "
+				+ String.format("%.2f", getCustomsFee())
+				+ ")";
+	}
+	
+	public Double totalPrice() {
+		return getPrice() + getCustomsFee();
+	}
 
 }
